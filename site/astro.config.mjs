439cd792275cf896026fb17147ff7800cdce85c6
Nicola Del Verme <nicoladelverme@MacBook-Air-6.local>
2026-01-09 20:32:45 -0500

    Use Astro GitHub Pages adapter

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
+import github from "@astrojs/github-pages";
 
 export default defineConfig({
-  site: "https://nickdv6.github.io",
-  base: "/",
   output: "static",
+  adapter: github(),
 });
