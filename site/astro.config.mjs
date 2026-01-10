13c15b3462caf6a61b6ec99e7f15d31d067a6e77
Nicola Del Verme <nicoladelverme@MacBook-Air-6.local>
2026-01-09 20:40:04 -0500

    Fix base path and routing for GitHub Pages

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import github from "@astrojs/github-pages";
 
 export default defineConfig({
+  site: "https://nickdv6.github.io",
+  base: "/raf-ai-v1",
   output: "static",
   adapter: github(),
 });
+
